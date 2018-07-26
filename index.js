f6cb01ea8f5569df245ab2cfa37db4d1c71f26a8
Lo Leung <kalefive.me@gmail.com>
2018-07-26 00:20:43 -0400

    attempt - not working

--- index.js
+++ index.js
@@ -6,10 +6,23 @@ exports.handler = function(event, context, callback) {
   // branches/[branchName]/[fileName]
   let key = event.Records[0].s3.object.key;
   let key_for_blinkdiff = key.split("/").join("_")
-  Promise.all([s3Helpers.pullNewBranchS3Image(bucket, key), s3Helpers.pullMasterS3Image(bucket, key)])
-    .then(function(buffers) {
+
+  let global_branch_buffer = null;
+  let global_master_buffer = null;
+
+  s3Helpers.pullNewBranchS3Image(bucket, key)
+    .then(function(branch_buffer) {
+      global_branch_buffer = branch_buffer
+      return s3Helpers.pullMasterS3Image(bucket, key)
+    }).then(function(master_buffer) {
+      console.log('3')
+      if (master_buffer != null) { return master_buffer }
+      console.log("file not found in MASTER - creating new file in master")
+      return s3Helpers.uploadBranchFileToMaster(bucket, key)
+    }).then(function(master_buffer) {
+      global_master_buffer = master_buffer
       console.log("Right before running blinkDiff")
-      return blinkDiff.runBufferInput(buffers, key_for_blinkdiff)
+      return blinkDiff.runBufferInput([global_master_buffer, global_branch_buffer], key_for_blinkdiff)
     }).then(function() {
       console.log("Right before uploading to S3")
       return s3Helpers.uploadDiffToS3(bucket, key_for_blinkdiff)
